97971a0f8054cd3eaef734ff239ef89c27b58177
JOY <joybosu@gmail.com>
2022-02-25 11:10:58 +0530

    Add check employe Program file

---
+++ CheckEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpsEmployeeWage
+{
+    public class CheckEmployee
+    {
+        ///<summary>
+        /// UC1- the employee present or not.
+        ///</summary>
+        public void EmployeePresentOrNot()
+        {
+            Random random = new Random();
+            int number = random.Next(2);
+
+            if (number == 1)
+            {
+                Console.WriteLine("Employee is Present :");
+            }
+            else
+            {
+                Console.WriteLine("Employee is not present :");
+            }
+
+        }
+    }
+}
