3e114d9f6649c82af9124907fefb0e9d8ddf140d
JOY <joybosu@gmail.com>
2022-02-27 21:50:11 +0530

    upload bilder problom

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpsEmployeeWage;
+using System;
 
 namespace EmployWageMulti
 {
@@ -6,11 +7,18 @@ namespace EmployWageMulti
     {
         static void Main(string[] args)
         {
-            CompaneyWage EmployWageobj = new CompaneyWage();
-            EmployWageobj.CalEmpWage();
-            Console.WriteLine(EmployWageobj.Companey);
-            Console.WriteLine(EmployWageobj.Companey_Name);
-            Console.WriteLine(EmployWageobj.Companey_Two);
+            //CompaneyWage EmployWageobj = new CompaneyWage();
+            //EmployWageobj.CalEmpWage();
+            //Console.WriteLine(EmployWageobj.Companey);
+            //Console.WriteLine(EmployWageobj.Companey_Name);
+            //Console.WriteLine(EmployWageobj.Companey_Two);
+
+            EmpWageBuldierProb obj = new EmpWageBuldierProb("Dmart",20,2,10);
+            EmpWageBuldierProb relaince = new EmpWageBuldierProb("Relaince", 10, 4, 20);
+            obj.computeEmpWage();
+            Console.WriteLine(obj.toString());
+            relaince.computeEmpWage();
+            Console.WriteLine(relaince.toString());
         }
     }
 }
